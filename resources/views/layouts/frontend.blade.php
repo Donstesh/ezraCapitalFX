bc0852d10c87f2110f09226aabfb47c3f92aa384
Donstesh <stephenshifoko@gmail.com>
2024-09-12 00:32:22 +0300

    front end layout

---
+++ resources/views/layouts/frontend.blade.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Ez Capital FX Live || Trade Forex, CFDs and indices</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,,500,600,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+	  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	      <a class="navbar-brand" href="{{url('/')}}">EZRA CAPITAL FX LIVE</a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	          <li class="nav-item active"><a href="{{url('/')}}" class="nav-link">HOME</a></li>
+	          <li class="nav-item"><a href="{{url('faq')}}" class="nav-link">FAQ</a></li>
+	          <li class="nav-item"><a href="{{url('about-us')}}" class="nav-link">ABOUT US</a></li>
+	          <li class="nav-item"><a class="nav-link" href="{{url('contact-us')}}">CONTACT</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+            @yield('content')
+            <footer class="ftco-footer ftco-bg-dark ftco-section">
+  <div class="container">
+    <div class="row mb-5 d-flex flex-wrap">
+      <div class="col-md-2">
+        <div class="ftco-footer-widget mb-4">
+          <h2 class="ftco-heading-2">Contact</h2>
+          <p>Email</p>
+        </div>
+      </div>
+      <div class="col-md-2">
+        <div class="ftco-footer-widget mb-4 ml-md-5">
+          <h2 class="ftco-heading-2">Products</h2>
+          <ul class="list-unstyled">
+            <li><a href="#" class="py-2 d-block">Buy Crypto</a></li>
+            <li><a href="#" class="py-2 d-block">Copy Trading</a></li>
+            <li><a href="#" class="py-2 d-block">Forex Trading Guide</a></li>
+            <li><a href="#" class="py-2 d-block">Crypto Trading Guide</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-2">
+        <div class="ftco-footer-widget mb-4">
+          <h2 class="ftco-heading-2">Resources</h2>
+          <ul class="list-unstyled">
+            <li><a href="#" class="py-2 d-block">About Us</a></li>
+            <li><a href="#" class="py-2 d-block">Cookie Policy</a></li>
+            <li><a href="#" class="py-2 d-block">Privacy Policy</a></li>
+            <li><a href="#" class="py-2 d-block">Risk Disclosure</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-2">
+        <div class="ftco-footer-widget mb-4">
+          <h2 class="ftco-heading-2">Indices Trading</h2>
+          <ul class="list-unstyled">
+            <li><a href="#" class="py-2 d-block">DE 30 Trading</a></li>
+            <li><a href="#" class="py-2 d-block">UK 100 Trading</a></li>
+            <li><a href="#" class="py-2 d-block">S&P 500 Trading</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-2">
+        <div class="ftco-footer-widget mb-4">
+          <h2 class="ftco-heading-2">Forex Trading</h2>
+          <ul class="list-unstyled">
+            <li><a href="#" class="py-2 d-block">USD/JPY Chart</a></li>
+            <li><a href="#" class="py-2 d-block">EUR/USD Chart</a></li>
+            <li><a href="#" class="py-2 d-block">GBP/USD Chart</a></li>
+            <li><a href="#" class="py-2 d-block">USD/CAD Chart</a></li>
+            <li><a href="#" class="py-2 d-block">AUD/USD Chart</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-2">
+        <div class="ftco-footer-widget mb-4">
+          <h2 class="ftco-heading-2">Crypto Trading</h2>
+          <ul class="list-unstyled">
+            <li><a href="#" class="py-2 d-block">Bitcoin Trading</a></li>
+            <li><a href="#" class="py-2 d-block">Litecoin Trading</a></li>
+            <li><a href="#" class="py-2 d-block">Algorand Trading</a></li>
+            <li><a href="#" class="py-2 d-block">Ethereum Trading</a></li>
+            <li><a href="#" class="py-2 d-block">Compound Trading</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/jquery.timepicker.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+
+  </body>
+</html>
