9132d416e5bbd8e0fd7b3eb50ec16d6ffce2d012
Donstesh <stephenshifoko@gmail.com>
2024-09-12 00:32:28 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about-us', function () {
+    return view('about');
+});
+
+Route::get('/contact-us', function () {
+    return view('contact');
+});
+Route::get('/faq', function () {
+    return view('faq');
+});
+
 Auth::routes();
 
 /*------------------------------------------
