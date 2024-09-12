7d2b658de248261a04bac1c73b4ba8d3fcbc8716
Donstesh <stephenshifoko@gmail.com>
2024-09-12 16:46:45 +0300

    otp mail

---
+++ resources/views/emails/otp.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your OTP</title>
+</head>
+<body>
+    <p>Hello,</p>
+    <p>Your one-time password (OTP) is: <strong>{{ $otp }}</strong></p>
+    <p>This OTP will expire in 10 minutes. Please use it to complete your registration.</p>
+</body>
+</html>
