79a0023bfce5e405fb627ffd4f85f66ca3929b87
Donstesh <stephenshifoko@gmail.com>
2024-09-18 03:00:58 +0300

    deposit confirmation mail

---
+++ resources/views/emails/deposit-confirmation.blade.php
@@ -0,0 +1,21 @@
+<x-mail::message>
+# Deposit Confirmation
+
+Dear {{ Auth::user()->f_name }} {{ Auth::user()->l_name }},
+
+Your deposit request has been successfully received. Please wait for confirmation. Here are the details:
+
+- **Amount**: {{ number_format($deposit->amount, 8) }} {{ strtoupper($deposit->deposit_method) }}
+- **Deposit Method**: {{ strtoupper($deposit->deposit_method) }}
+- **Transaction ID**: {{ $deposit->trx_id }}
+- **Status**: {{ $deposit->deposit_status }}
+
+You can track your deposit status from your dashboard.
+
+<x-mail::button :url="url('/home')">
+Go to Dashboard
+</x-mail::button>
+
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message>
