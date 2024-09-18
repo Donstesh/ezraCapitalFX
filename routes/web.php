a81ee0401f63144b47238eae926cd3d9b309e6cd
Donstesh <stephenshifoko@gmail.com>
2024-09-18 03:02:14 +0300

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\WalletController;
 use App\Http\Controllers\KycVerifyController;
 use App\Http\Controllers\TradeController;
 use App\Http\Controllers\WithdrawalController;
+use App\Http\Controllers\UserPlanController;
 use Illuminate\Support\Facades\Mail;
 
 Route::get('/', function () {
@@ -42,6 +43,7 @@ Route::get('/test-email', function () {
     }
 });
 
+Route::get('/api/crypto-prices', [HomeController::class, 'getCryptoPrices']);
 /*------------------------------------------
 --------------------------------------------
 All Normal Users Routes List
@@ -56,7 +58,7 @@ Route::middleware(['auth', 'user-access:user'])->group(function () {
     Route::get('/addresses', [WalletController::class, 'walletAddress'])->name('addresses');
     Route::post('/deposit', [WalletController::class, 'depositRequest'])->name('deposit.request');
 
-    Route::get('/plans', [HomeController::class, 'plansList'])->name('plans');
+    Route::get('/plans', [UserPlanController::class, 'index'])->name('plans');
     Route::get('/copy-trading', [HomeController::class, 'copyTrading'])->name('copy-trading');
     Route::get('/trading-room', [TradeController::class, 'index'])->name('trading-room');
     Route::get('/open-trade', [HomeController::class, 'openTrades'])->name('open-trade');
@@ -70,6 +72,9 @@ Route::middleware(['auth', 'user-access:user'])->group(function () {
     Route::post('/kyc', [KycVerifyController::class, 'personaDetails'])->name('kyc.store');
     Route::post('/selfie', [KycVerifyController::class, 'selfie'])->name('kyc.selfie');
     Route::post('/proof-of-residence', [KycVerifyController::class, 'proofOfResidence'])->name('kyc.proof-of-residence');
+
+    Route::post('/select-plan', [UserPlanController::class, 'store'])->name('selecet-plan');
+
 });
 
 /*------------------------------------------
@@ -79,7 +84,30 @@ All Admin Routes List
 --------------------------------------------*/
 Route::middleware(['auth', 'user-access:admin'])->group(function () {
 
-    Route::get('/admin/home', [HomeController::class, 'adminHome'])->name('admin.home');
+    Route::get('/admin/home', [HomeController::class, 'adminHome'])->name('adminHome');
+
+    Route::get('/adminHome', [HomeController::class, 'adminHome'])->name('admin.home');
+    Route::get('/new-orders', [HomeController::class, 'newOrders'])->name('admin.new-orders');
+    Route::put('/update-order', [HomeController::class, 'updateOrder'])->name('admin.updateOrder');
+    Route::put('/cancel-order', [HomeController::class, 'cancelOrder'])->name('admin.cancelOrder');
+    Route::get('/complete-orders', [HomeController::class, 'completeOrders'])->name('admin.complete-orders');
+    Route::get('/view-products', [HomeController::class, 'viewProducts'])->name('admin.view-products');
+    Route::post('/save-product', [HomeController::class, 'saveProduct'])->name('save-product.store');
+    Route::get('delete-product/{id}',[HomeController::class, 'deleteProduct'])->name('admin.product.delete');
+    Route::get('/user-deposits', [HomeController::class, 'userDeposit'])->name('admin.user-deposits');
+    Route::put('/update-status', [HomeController::class, 'updateStatus'])->name('admin.updateStatus');
+    Route::get('/category-setup', [HomeController::class, 'categorySetup'])->name('admin.category-setup');
+    Route::post('/save-m-category', [HomeController::class, 'saveMCategory'])->name('save-m-category.store');
+    Route::post('/save-category', [HomeController::class, 'saveCategory'])->name('save-category.store');
+    Route::get('delete-m-category/{id}',[HomeController::class, 'deleteMCategory'])->name('admin.m-category.delete');
+    Route::get('delete-category/{id}',[HomeController::class, 'deleteCategory'])->name('admin.category.delete');
+    Route::get('/view-wallets', [HomeController::class, 'viewWallets'])->name('admin.view-wallets');
+    Route::post('/save-wallet', [HomeController::class, 'saveWallet'])->name('save-wallet.store');
+    Route::get('delete-wallet/{id}',[HomeController::class, 'deleteWallet'])->name('admin.wallet.delete');
+    Route::get('/view-users', [HomeController::class, 'viewUsers'])->name('admin.view-users');
+    Route::get('delete-user/{id}',[HomeController::class, 'deleteUser'])->name('admin.user.delete');
+    Route::get('/site-setup',[HomeController::class, 'viewSetup'])->name('admin.site-setup');
+    Route::post('/support-settings', [HomeController::class, 'saveSupportSettings'])->name('admin.support.save');
 });
 
 /*------------------------------------------
